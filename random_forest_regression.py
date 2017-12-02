dcfac0c3ee06165397db37e2574934b45c792c65
Santanu Sarker <sarkersantanu@gmail.com>
2017-12-01 23:36:28 -0500

    Create random_forest_regression.py

---
+++ random_forest_regression.py
@@ -0,0 +1,39 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score, r2_score
+import statsmodels.formula.api as sm
+
+# Importing the dataset
+dataset = pd.read_csv('yelp_reviews_min.csv')
+numberOfFeatures = 12
+
+X = dataset.iloc[:, 1:numberOfFeatures].values
+y = dataset.iloc[:, numberOfFeatures].values.reshape(-1, 1)
+
+#Feature Scaling
+X_scaler = StandardScaler().fit(X)
+X_scaled = X_scaler.transform(X)
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size = 0.5, random_state = 0)
+
+#Fitting Random Forest Regression to the dataset
+randomforest = RandomForestRegressor(n_estimators = 21, random_state = 0)
+randomforest.fit(X_train, y_train)
+
+# Predicting on the test set 
+y_pred = randomforest.predict(X_test)
+regressionRes = y_pred.reshape(-1,1)
+
+# performance metrics
+rSquared = r2_score(regressionRes, y_test)
+adjustedRSquared = 1 - (1-rSquared)*(len(y)-1)/(len(y)-X.shape[1]-1)
+print(rSquared)
+print(adjustedRSquared)
